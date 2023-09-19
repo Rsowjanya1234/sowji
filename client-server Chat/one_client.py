f015a46bd07f283232b4877c31308ac7022a530b
Rsowjanya1234 <118812726+Rsowjanya1234@users.noreply.github.com>
2023-09-19 11:26:20 +0530

    Create one_client.py

--- client-server Chat/one_client.py
+++ client-server Chat/one_client.py
@@ -1,9 +1,11 @@
 import socket
 import json
 
+#open and read the server configuration from 'server_config.json'
 with open('server_config.json') as config_file:
     config = json.load(config_file)
-
+    
+#create a client socket an connect to the server
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = (config['ip'], config['port'])
 client_socket.connect(server_address)
@@ -15,7 +17,9 @@ while True:
     if message.lower() == 'exit':
         print("Connection closed by client.")
         break
+    #Receive and display the servers response
     response = client_socket.recv(1024).decode('utf-8')
     print("Server:", response)
-  
+    
+#Close the client socket when done  
 client_socket.close()
